a74f68fc1439067e6b95818c48a6157af81a5a30
Renan Santos <dosantosrenansilva@gmail.com>
2022-10-12 15:50:58 -0300

    Then it shows, in alphabetical order, the email of the employees whose salary exceeds a given amount provided by the user.

---
+++ EmployeePayslip/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayslip.Entities
+{
+    internal class Employee
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public Double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
